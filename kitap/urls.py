696efa5f843a758bc3d5ffec4d689f3eba75f46f
Aisayev Duman <duman200354@mail.ru>
2022-05-03 14:43:45 +0600

    with authentication

---
+++ kitap/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('authentication', views.authentication, name="authentication"), 
+    path('logout', views.logoutUser, name="logout"), 
+    path('', views.index, name="index"), 
+    path('about', views.about, name="about"),
+    path('categories', views.categories, name="categories"),
+    path('profile', views.profile, name="profile"),
+    path('faq', views.faq, name="faq"),
+]
