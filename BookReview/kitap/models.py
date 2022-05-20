a839fb5028d4abc66f76d688a7c25724b6d3c033
Aisayev Duman <duman200354@mail.ru>
2022-05-20 15:01:52 +0600

    profile page done

--- BookReview/kitap/models.py
+++ BookReview/kitap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Faq(models.Model):
@@ -14,6 +15,16 @@ class Category(models.Model):
     cat_info = models.TextField()
     cat_image = models.ImageField(upload_to='img', blank=True, null=True)
     
-    
     def __str__(self):
         return self.cat_name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=16)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    
--- BookReview/kitap/urls.py
+++ BookReview/kitap/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('authentication', views.authentication, name="authentication"), 
-    path('logout', views.logoutUser, name="logout"), 
-    path('', views.index, name="index"), 
+    path('authentication', views.authentication, name="authentication"),
+    path('logout', views.logoutUser, name="logout"),
+    path('', views.index, name="index"),
     path('about', views.about, name="about"),
     path('categories', views.categories, name="categories"),
-    path('profile', views.profile, name="profile"),
+    path('profile/<int:pk>', views.profile, name="profile"),
+    path('edit_profile/<int:pk>', views.edit_profile, name="edit_profile"),
     path('faq', views.faq, name="faq"),
 ]
